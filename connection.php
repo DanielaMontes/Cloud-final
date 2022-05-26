f52a94c0cc42573660a129618886bee42b89ba78
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 17:12:36 -0500

    dns

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conex = mysqli_connect("10.1.0.6:3306", "dani", "dani","mv1") or die('Could not connect the database : Username or password incorrect');
+$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1") or die('Could not connect the database : Username or password incorrect');
 echo("Conexion Exitosa");
 // $conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1");
 // $conex = mysqli_connect("localhost", "root", "","alumnos");
