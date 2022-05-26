133c5edb2a7967f794bbd0c2bb05e882e8871778
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 13:51:28 -0500

    agrega puerto

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com", "vic", "vic","mv1");
+$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "vic", "vic","mv1");
 // $conex = mysqli_connect("localhost", "root", "","alumnos");
 
 ?>
