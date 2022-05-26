2cf8339a65c9ed6a49f8c763ec6a3ec773ca520b
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 14:52:11 -0500

    cambio de puerto

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1");
+$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:443", "dani", "dani","mv1");
 // $conex = mysqli_connect("localhost", "root", "","alumnos");
 
 ?>
