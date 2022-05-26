2ebd31dda493152bcbe57f19f742981d76005432
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 15:53:43 -0500

    ip publica

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 
-$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1");
+$conex = mysqli_connect("40.86.112.194", "dani", "dani","mv1");
+// $conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1");
 // $conex = mysqli_connect("localhost", "root", "","alumnos");
 
 ?>
