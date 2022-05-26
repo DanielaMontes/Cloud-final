c8f4564cc2c22c58efed620114d02c1dc02e16ff
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 14:30:55 -0500

    cambio de usuario

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "jona", "jona","mv1");
+$conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1");
 // $conex = mysqli_connect("localhost", "root", "","alumnos");
 
 ?>
