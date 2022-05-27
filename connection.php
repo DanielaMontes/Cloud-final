72e487c9796ca3ce8265e957fd26ca5956697d59
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-05-26 20:42:30 -0500

    d

--- connection.php
+++ connection.php
@@ -1,7 +1,8 @@
 <?php
 
-$conex = mysqli_connect("10.1.0.6:3306/mv1", "dani", "dani","mv1") or die('Could not connect the database : Username or password incorrect');
-echo("Conexion Exitosa");
+$conex = mysqli_connect("10.1.0.6:3306/mv1", "dani", "dani","mv1") 
+    // or die('Could not connect the database : Username or password incorrect');
+// echo("Conexion Exitosa");
 // $conex = mysqli_connect("cloudmv.centralus.cloudapp.azure.com:3306", "dani", "dani","mv1");
 // $conex = mysqli_connect("localhost", "root", "","alumnos");
 
